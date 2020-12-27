8cdf3df243fe2996bb866e3c559263420f905ee2
Alex <sepp.amsl@protonmail.com>
2020-12-27 17:36:16 +0100

    Added Xamarin.forms project. ʕ •ᴥ•ʔ

---
+++ src/Nindo.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Nindo.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Nindo.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ src/Nindo.Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Nindo.Mobile.Models;
+using Nindo.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nindo.Mobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
