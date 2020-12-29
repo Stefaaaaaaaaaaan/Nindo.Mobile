dfa0dae55caaef644600847df2374653c1050ea7
Alex <sepp.amsl@protonmail.com>
2020-12-30 00:09:45 +0100

    Added new project.

---
+++ src/Nindo.Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Nindo.Mobile.ViewModels;
+using Nindo.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Nindo.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
