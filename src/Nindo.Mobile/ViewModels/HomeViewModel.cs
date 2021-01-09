abe7ca2092068fc9203b493ef80bcbd8631ae63a
Alex <sepp.amsl@protonmail.com>
2021-01-09 17:00:54 +0100

    Added LazyViews for home page.

--- src/Nindo.Mobile/ViewModels/HomeViewModel.cs
+++ src/Nindo.Mobile/ViewModels/HomeViewModel.cs
@@ -23,6 +23,11 @@ namespace Nindo.Mobile.ViewModels
         public HomeViewModel()
         {
             Title = "Nindo";
+            YoutubeViewModel = new YoutubeSmallViewModel();
+            InstagramViewModel = new InstagramSmallViewModel();
+            TiktokViewModel = new TiktokSmallViewModel();
+            TwitterViewModel = new TwitterSmallViewModel();
+            TwitchViewModel = new TwitchSmallViewModel();
         }
 
         private int _selectedViewModelIndex = 0;
@@ -31,6 +36,41 @@ namespace Nindo.Mobile.ViewModels
         {
             get => _selectedViewModelIndex;
             set => SetAndRaise(ref _selectedViewModelIndex, value);
+        }
+
+        private YoutubeSmallViewModel _youtubeViewModel;
+        public YoutubeSmallViewModel YoutubeViewModel
+        {
+            get => _youtubeViewModel;
+            set => SetAndRaise(ref _youtubeViewModel, value);
+        }
+
+        private InstagramSmallViewModel _instagramViewModel;
+        public InstagramSmallViewModel InstagramViewModel
+        {
+            get => _instagramViewModel;
+            set => SetAndRaise(ref _instagramViewModel, value);
+        }
+
+        private TiktokSmallViewModel _tiktokViewModel;
+        public TiktokSmallViewModel TiktokViewModel
+        {
+            get => _tiktokViewModel;
+            set => SetAndRaise(ref _tiktokViewModel, value);
+        }
+
+        private TwitterSmallViewModel _twitterViewModel;
+        public TwitterSmallViewModel TwitterViewModel
+        {
+            get => _twitterViewModel;
+            set => SetAndRaise(ref _twitterViewModel, value);
+        }
+
+        private TwitchSmallViewModel _twitchViewModel;
+        public TwitchSmallViewModel TwitchViewModel
+        {
+            get => _twitchViewModel;
+            set => SetAndRaise(ref _twitchViewModel, value);
         }
 
         protected bool SetAndRaise<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
